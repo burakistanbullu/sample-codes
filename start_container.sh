f6445fbd172c5f0b1649689cbc77723c5d1a4166
Burak Istanbullu <155552983+burakistanbullu@users.noreply.github.com>
2025-12-30 12:49:15 +0300

    temp

--- start_container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-docker pull burakistanbullu/simple-python-flask-app:latest
-
-docker run -d --name simple-python-flask-app -p 5000:5000 burakistanbullu/simple-python-flask-app:latest
